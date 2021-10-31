4c795e8907e1b024c275b7a893a30e3aca51967c
Joe Theriault <joseph@yankoismedia.com>
2021-10-31 14:55:46 -0400

    Add tests for removeObjectEntries
    Target ES2020 to allow spread operator

--- src/object-entries-filter/index.js
+++ src/object-entries-filter/index.js
@@ -1,6 +1,14 @@
-function removeObjectEntries (object, exclude) {
-    return Object.fromEntries(Object.entries(object)
-        .filter(([key, value]) => exclude[key] !== value)
+/**
+ * Remove the matching entries from the object.
+ * An entry matches if its name and value is the same.
+ *
+ * @param {object} baseObject - The object to base the return value on
+ * @param {object} entries - An object of entries to remove
+ * @return {object} - A copy of `baseObject` stripped of properties matching `entries`
+ */
+function removeObjectEntries (baseObject, entries) {
+    return Object.fromEntries(Object.entries(baseObject)
+        .filter(([key, value]) => entries[key] !== value)
     );
 }
 
