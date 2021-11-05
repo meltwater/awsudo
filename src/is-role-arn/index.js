999350cb7037ee94c4a17f77b4a22d24374ca996
Joe Theriault <joe.theriault@meltwater.com>
2021-11-04 22:13:11 -0400

    Revert "Merge remote-tracking branch 'origin/master' into aws-partitions"
    This reverts commit b2a658da61408ede3c681e11991eeb606f2765cd, reversing
    changes made to bc20cbf83ebc9dd64b96ec418ebe54376df56ad5.

--- src/is-role-arn/index.js
+++ src/is-role-arn/index.js
@@ -1,13 +1,9 @@
 function isRoleArn (roleArn) {
-<<<<<<< HEAD
     return [
         /^arn:aws:iam/,
         /^arn:aws-cn:iam/,
         /^arn:aws-us-gov:iam/
     ].some(regex => regex.test(roleArn));
-=======
-    return /^arn:aws:iam/.test(roleArn);
->>>>>>> origin/master
 }
 
 module.exports = {
